18bb559db9893551fb1a50e0436a73b96b0465cb
Joe Chandler <joseph-chandler@sky.com>
2025-06-15 02:02:56 +0100

    Quick Fix

--- src/MicroServices/PriceManager/PriceManager.Api/MessageHandlers/SubscribeCommandHandler.cs
+++ src/MicroServices/PriceManager/PriceManager.Api/MessageHandlers/SubscribeCommandHandler.cs
@@ -5,17 +5,12 @@ using Pricing.Application.Messages;
 namespace PriceManager.Api.MessageHandlers;
 
 public class SubscribeCommandHandler(ILogger<SubscribeCommandHandler> _logger) 
-    : IMessageHandler<SubscribeCommand>
+    : MessageHandler<SubscribeCommand>
 {
-    public Task HandleAsync(SubscribeCommand message)
+    public override Task HandleAsync(SubscribeCommand message)
     {
         _logger.LogInformation("Subscribe command received: {0}", message);
         
         return Task.CompletedTask;
-    }
-
-    public Task HandleAsync(IMessage message)
-    {
-        return HandleAsync(message as SubscribeCommand);
     }
 }
